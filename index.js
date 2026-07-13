73a8b01553e43f6d4ab8610108dfcd9ac50461f5
Riccardo <131913894+riccardoq76@users.noreply.github.com>
2026-07-13 11:34:11 +0200

    Update global variables for Homebridge v2 compatibility
    Expose Formats, Units, and Perms from homebridge.hap.

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const {
 module.exports = (homebridge) => {
   global.Service = homebridge.hap.Service;
   global.Characteristic = homebridge.hap.Characteristic;
+  // Homebridge v2 / HAP-NodeJS v1+ removed the Formats/Units/Perms enums that used to
+  // live as static properties on Characteristic. They now live on hap instead, so we
+  // expose them the same way the rest of this plugin exposes Service/Characteristic.
+  global.Formats = homebridge.hap.Formats;
+  global.Units = homebridge.hap.Units;
+  global.Perms = homebridge.hap.Perms;
   // eslint-disable-next-line global-require
   global.FakeGatoHistoryService = require('fakegato-history')(homebridge);
 
